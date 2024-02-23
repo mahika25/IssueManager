1a2ba00b12fed5f5c1a7972788e30446be639ce4
mkpatil <mkpatil@ncsu.edu>
2024-02-22 21:09:36 -0500

    Added Javadoc to all classes

--- Project1/src/edu/ncsu/csc216/issue_manager/model/manager/IssueList.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/manager/IssueList.java
@@ -10,38 +10,79 @@ import edu.ncsu.csc216.issue_manager.model.issue.Issue;
 import edu.ncsu.csc216.issue_manager.model.issue.Issue.IssueType;
 
 /**
- * 
+ * Class to manage a list of issues 
+ * @author Mahika Patil
  */
 public class IssueList {
 	
+	/** Counter to generate issue Id */
 	private int counter;
 	
-	public IssueList() {}
+	/** Constructs the IssueList object */
+	public IssueList() { }
 	
+	/** 
+	 * Add issue to issue list
+	 * @param issueType type of issue being added
+	 * @param summary summary of issue to be added
+	 * @param note note for issue to be added
+	 * @return the issue id
+	 */
 	public int addIssue (IssueType issueType, String summary, String note) {
 		return 0;
 	}
 	
-	public void addIssues(List<Issue> issues) {}
+	/**
+	 * Add multiple issues to a list
+	 * @param issues issues to be added
+	 */
+	public void addIssues(List<Issue> issues) { }
 	
-	private void addIssue(Issue issue) {}
+	/**
+	 * Add existing issue 
+	 * @param issue issue to be added
+	 */
+	private void addIssue(Issue issue) { }
 	
+	/**
+	 * Get list of issues
+	 * @return list of all issues
+	 */
 	public List<Issue> getIssues(){
 		return null;
 	}
 	
+	/**
+	 * Get list of issues of a certain type
+	 * @param type of issues to get
+	 * @return list of all issues
+	 */
 	public List<Issue> getIssuesByType(String type){
 		return null;
 	}
 	
+	/**
+	 * Get an issue by its id
+	 * @param id id to get
+	 * @return issue of the specified id
+	 */
 	public Issue getIssueById(int id){
 		return null;
 	}
 	
+	/**
+	 * executes a command on a certain issue
+	 * @param id id of issue to execute command on
+	 * @param command command to execute
+	 */
 	public void executeCommand(int id, Command command) {
 		
 	}
 	
+	/**
+	 * deletes an issue
+	 * @param id id of issue to delete
+	 */
 	public void deleteIssueById(int id) {
 		
 	}
--- Project1/src/edu/ncsu/csc216/issue_manager/model/manager/IssueManager.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/manager/IssueManager.java
@@ -4,46 +4,96 @@ import edu.ncsu.csc216.issue_manager.model.command.Command;
 import edu.ncsu.csc216.issue_manager.model.issue.Issue;
 import edu.ncsu.csc216.issue_manager.model.issue.Issue.IssueType;
 
+/**
+ * Manages overall system
+ * @author Mahika Patil
+ */
 public class IssueManager {
 	
-	private IssueManager(){}
+	/** Constructs the issue manager object */
+	private IssueManager(){ }
 	
+	/**
+     * Gets the  instance of the IssueManager.
+     * @return IssueManager instance.
+     */
 	public static IssueManager getInstance() {
 		return null;
 	}
 	
+	/**
+     * Saves the current issue list to a file
+     * @param filename The name of the file to save to
+     */
+
 	public void saveIssuesToFile(String filename) {
 		
 	}
 	
+	/**
+     * Loads an issue list from a file
+     * @param filename The name of the file to load from
+     */
 	public void loadIssuesFromFile(String filename) {
 			
 	}
 	
+	/**
+     * creates a new empty issue list
+     */
 	public void createNewIssueList() {
 		
 	}
 	
+	/**
+     * gets the current issue list as a two-dimensional array
+     * @return an array representation of the issue list
+     */
 	public Object[][] getIssueListAsArray() {
 		return null;
 	}
 	
+	/**
+     * gets the current issue list for a certain issue type as a two-dimensional array
+     * @param type type of issue to get
+     * @return an array representation for the issues of a certain type
+     */
 	public Object[][] getIssueListAsArrayByIssueType(String type) {
 		return null;
 	}
 	
+	/**
+     * gets an issue by its id
+     * @param id The id of the issue to get
+     * @return The issue with the specified id
+     */
 	public Issue getIssueById(int id) {
 		return null;
 	}
 	
+	/**
+	 * executes a command on a certain issue
+	 * @param id id of issue to execute command on
+	 * @param command command to execute
+	 */
 	public void executeCommand(int id, Command command) {
 		
 	}
 	
+	/**
+	 * deletes an issue
+	 * @param id id of issue to delete
+	 */
 	public void deleteIssueById(int id) {
 		
 	}
 	
+	/** 
+	 * Add issue to issue list
+	 * @param issueType type of issue being added
+	 * @param summary summary of issue to be added
+	 * @param note note for issue to be added
+	 */
 	public void addIssueToList(IssueType issueType, String summary, String note) {
 		
 	}
