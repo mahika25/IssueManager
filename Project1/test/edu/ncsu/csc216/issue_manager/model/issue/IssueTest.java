64171f61a4ba3aa8ff62175e3ae21e6706368d88
mkpatil <mkpatil@ncsu.edu>
2024-03-06 16:10:37 -0500

    [Testing] Improving coverage for Issue class

--- Project1/test/edu/ncsu/csc216/issue_manager/model/issue/IssueTest.java
+++ Project1/test/edu/ncsu/csc216/issue_manager/model/issue/IssueTest.java
@@ -164,10 +164,20 @@ public class IssueTest {
 		Command c6 = new Command(CommandValue.REOPEN, "mk", Resolution.DUPLICATE, "note");
 		issue.update(c6);
 		assertEquals("Working", issue.getStateName());
+		
+	}
 	
+	/** Tests testUpdate method */
+	@Test
+	public void testUpdate7() {
+		ArrayList<String> notes = new ArrayList<>();
+		notes.add(note);
+		Issue issue = new Issue(id, "Closed", "Enhancement", "summary", "", false, "Fixed", notes);
+		assertEquals("Closed", issue.getStateName());
 		
-		
-
+		Command c6 = new Command(CommandValue.REOPEN, "", Resolution.DUPLICATE, "note");
+		issue.update(c6);
+		assertEquals("New", issue.getStateName());
 		
 	}
 	
@@ -183,6 +193,29 @@ public class IssueTest {
 		assertThrows(UnsupportedOperationException.class, () -> issue.update(c2));
 		
 	}
+	
+	/** Tests testUpdate method that throws an assertion*/
+	@Test
+	public void testUpdateFail2() {
+		ArrayList<String> notes = new ArrayList<>();
+		notes.add(note);
+		Issue issue = new Issue(id, "New", "Bug", "summary", "", true, "Duplicate", notes);
+		assertEquals("New", issue.getStateName());
+		
+		Command c = new Command(CommandValue.RESOLVE, null, Resolution.DUPLICATE, "note");
+		issue.update(c);
+		assertEquals("Closed", issue.getStateName());
+		
+		Command c2 = new Command(CommandValue.REOPEN, null, Resolution.DUPLICATE, "note2");
+		issue.update(c2);
+		assertEquals("Confirmed", issue.getStateName());
+		
+		Command c3 = new Command(CommandValue.RESOLVE, null, Resolution.DUPLICATE, "note");
+		assertThrows(UnsupportedOperationException.class, () -> issue.update(c3));
+		
+		
+	}
+	
 	
 	
 	
